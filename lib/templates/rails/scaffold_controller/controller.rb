d401977234783da12a0ec5cf11d0de043e2467d4
christian <christian.quentin@transparence-c.com>
2017-05-26 10:53:12 +0200

    préparation du scaffolding pour fonctionnement avec InheritedResources. @0h15

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -4,6 +4,16 @@ require_dependency "<%= namespaced_file_path %>/application_controller"
 <% end -%>
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < CrudController
+  #custom_actions :resource => [:cloner]
+
+  def attrs_for_index
+    [<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>]
+  end
+
+  def attrs_for_form
+    [<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>]
+  end
+
   # GET <%= route_url %>
   def index
     if (@filtre && !@filtre.empty?)
@@ -37,14 +47,21 @@ class <%= controller_class_name %>Controller < CrudController
     super(notice: <%= "'#{human_name} supprimé.'" %>)
   end
 
-  private
-    # Only allow a trusted parameter "white list" through.
-    def <%= "#{singular_table_name}_params" %>
-      <%- if attributes_names.empty? -%>
-      params[:<%= singular_table_name %>]
-      <%- else -%>
-      params.require(:<%= singular_table_name %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
-      <%- end -%>
-    end
+  # Filtre les opérations du stock en fonction d'un terme recherché
+  # - On limite à 100 résultats
+  def rechercher
+    @<%= plural_table_name %> = <%= orm_class %>.filtre_par(@filtre).limit(100)
+    render @<%= plural_table_name %>
+  end
+
+private
+  # Only allow a trusted parameter "white list" through.
+  def <%= "#{singular_table_name}_params" %>
+    <%- if attributes_names.empty? -%>
+    params[:<%= singular_table_name %>]
+    <%- else -%>
+    params.require(:<%= singular_table_name %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
+    <%- end -%>
+  end
 end
 <% end -%>
