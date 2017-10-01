fb9e8366d82575cce6859b970c08ceda2cbc21c4
Christian <christian.quentin@transparence-c.com>
2017-10-01 18:47:45 +0200

    inutile car ces infos sont dans le fichier de config /etc/ssmtp/ssmtp.conf

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,6 +2,4 @@ ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.sendmail_settings = {
   :location => '/usr/sbin/sendmail',
   :arguments => '-i',
-  :user_name => 'christian.quentin@transparence-c.com',
-  :password => 'wxcvb022'
 }
