77a8799b843382182b564988dbab35f527d6d8b0
Architecte du web <architecteduweb@localhost.localdomain>
2017-10-11 22:34:05 +0200

    indication mot de passe pour les neuneu. @0h20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 protected
 
    def authenticate
-     authenticate_or_request_with_http_basic do |username, password|
+     authenticate_or_request_with_http_basic("Veuillez saisir utilisateur et mot de passe (identiques). Indication : 2018raid en re-organisant les lettres avant les chiffres") do |username, password|
        username == "raid2018" && password == "raid2018"
      end
    end
